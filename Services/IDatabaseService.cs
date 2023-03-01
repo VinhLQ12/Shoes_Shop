ffb7e285338c1a2ddda18222877fe8141da4a944
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-03-02 00:39:25 +0700

    vinhlq_02032023_1
    Add Template

--- Services/IDatabaseService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shoes_Shop.Services {
-    public interface IDatabaseService {
-        T GetById<T>(int id) where T : class;
-        void Add<T>(T entity) where T : class;
-        void Update<T>(T entity) where T : class;
-        void Delete<T>(T entity) where T : class;
-    }
-}
